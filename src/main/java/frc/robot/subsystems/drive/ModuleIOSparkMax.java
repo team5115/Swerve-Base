e9ed40c852612cf522fa42416c86daddea303e18
AvidCoder27 <gilireeves@gmail.com>
2024-04-12 18:34:56 -0400

    formatting?

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSparkMax.java
@@ -19,132 +19,132 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import frc.robot.Constants.SwerveConstants;
 
 /**
- * Module IO implementation for SparkMax drive motor controller, SparkMax turn
- * motor controller (NEO or NEO 550), and analog absolute encoder connected to
- * the RIO
- * <p>
- * NOTE: This implementation should be used as a starting point and adapted to
- * different hardware configurations (e.g. If using a CANcoder, copy from
- * "ModuleIOTalonFX")
- * <p>
- * To calibrate the absolute encoder offsets, point the modules straight (such
- * that forward motion on the drive motor will propel the robot forward) and
- * copy the reported values from the absolute encoders using AdvantageScope.
- * These values are logged under "/Drive/ModuleX/TurnAbsolutePositionRad"
+ * Module IO implementation for SparkMax drive motor controller, SparkMax turn motor controller (NEO
+ * or NEO 550), and analog absolute encoder connected to the RIO
+ *
+ * <p>NOTE: This implementation should be used as a starting point and adapted to different hardware
+ * configurations (e.g. If using a CANcoder, copy from "ModuleIOTalonFX")
+ *
+ * <p>To calibrate the absolute encoder offsets, point the modules straight (such that forward
+ * motion on the drive motor will propel the robot forward) and copy the reported values from the
+ * absolute encoders using AdvantageScope. These values are logged under
+ * "/Drive/ModuleX/TurnAbsolutePositionRad"
  */
 public class ModuleIOSparkMax implements ModuleIO {
-    // Gear ratios for SDS MK4i L2, adjust as necessary
+  // Gear ratios for SDS MK4i L2, adjust as necessary
 
-    private final CANSparkMax driveSparkMax;
-    private final CANSparkMax turnSparkMax;
+  private final CANSparkMax driveSparkMax;
+  private final CANSparkMax turnSparkMax;
 
-    private final RelativeEncoder driveEncoder;
-    private final AbsoluteEncoder turnEncoder;
+  private final RelativeEncoder driveEncoder;
+  private final AbsoluteEncoder turnEncoder;
 
-    private final Rotation2d absoluteEncoderOffset;
+  private final Rotation2d absoluteEncoderOffset;
 
-    public ModuleIOSparkMax(int index) {
-        int driveId = -1;
-        int turnId = -1;
-        double angularOffsetDegrees = -1;
+  public ModuleIOSparkMax(int index) {
+    int driveId = -1;
+    int turnId = -1;
+    double angularOffsetDegrees = -1;
 
-        switch (index) {
-        case 0: // Front Left
-            driveId = SwerveConstants.FRONT_LEFT_DRIVE_ID;
-            turnId = SwerveConstants.FRONT_LEFT_TURN_ID;
-            angularOffsetDegrees = 90;
-            break;
-        case 1: // Front Right
-            driveId = SwerveConstants.FRONT_RIGHT_DRIVE_ID;
-            turnId = SwerveConstants.FRONT_RIGHT_TURN_ID;
-            angularOffsetDegrees = 180;
-            break;
-        case 2: // Back Left
-            driveId = SwerveConstants.BACK_LEFT_DRIVE_ID;
-            turnId = SwerveConstants.BACK_LEFT_TURN_ID;
-            angularOffsetDegrees = 0;
-            break;
-        case 3: // Back Right
-            driveId = SwerveConstants.BACK_RIGHT_DRIVE_ID;
-            turnId = SwerveConstants.BACK_RIGHT_TURN_ID;
-            angularOffsetDegrees = 270;
-            break;
-        default:
-            throw new RuntimeException("Invalid module index");
-        }
-
-        driveSparkMax = new CANSparkMax(driveId, MotorType.kBrushless);
-        turnSparkMax = new CANSparkMax(turnId, MotorType.kBrushless);
-        absoluteEncoderOffset = Rotation2d.fromDegrees(angularOffsetDegrees);
-
-        driveSparkMax.restoreFactoryDefaults();
-        turnSparkMax.restoreFactoryDefaults();
-
-        driveSparkMax.setCANTimeout(250);
-        turnSparkMax.setCANTimeout(250);
-
-        driveEncoder = driveSparkMax.getEncoder();
-        turnEncoder = turnSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
-
-        turnSparkMax.setInverted(SwerveConstants.TurningEncoderInverted);
-        driveSparkMax.setSmartCurrentLimit(SwerveConstants.DrivingMotorCurrentLimit);
-        turnSparkMax.setSmartCurrentLimit(SwerveConstants.TurningMotorCurrentLimit);
-        driveSparkMax.enableVoltageCompensation(12.0);
-        turnSparkMax.enableVoltageCompensation(12.0);
-
-        driveEncoder.setPosition(0.0);
-        driveEncoder.setMeasurementPeriod(10);
-        driveEncoder.setAverageDepth(2);
-
-        turnEncoder.setAverageDepth(2);
-
-        driveSparkMax.setCANTimeout(0);
-        turnSparkMax.setCANTimeout(0);
-
-        driveSparkMax.setIdleMode(SwerveConstants.DrivingMotorIdleMode);
-        turnSparkMax.setIdleMode(SwerveConstants.TurningMotorIdleMode);
-
-        driveSparkMax.burnFlash();
-        turnSparkMax.burnFlash();
+    switch (index) {
+      case 0: // Front Left
+        driveId = SwerveConstants.FRONT_LEFT_DRIVE_ID;
+        turnId = SwerveConstants.FRONT_LEFT_TURN_ID;
+        angularOffsetDegrees = 90;
+        break;
+      case 1: // Front Right
+        driveId = SwerveConstants.FRONT_RIGHT_DRIVE_ID;
+        turnId = SwerveConstants.FRONT_RIGHT_TURN_ID;
+        angularOffsetDegrees = 180;
+        break;
+      case 2: // Back Left
+        driveId = SwerveConstants.BACK_LEFT_DRIVE_ID;
+        turnId = SwerveConstants.BACK_LEFT_TURN_ID;
+        angularOffsetDegrees = 0;
+        break;
+      case 3: // Back Right
+        driveId = SwerveConstants.BACK_RIGHT_DRIVE_ID;
+        turnId = SwerveConstants.BACK_RIGHT_TURN_ID;
+        angularOffsetDegrees = 270;
+        break;
+      default:
+        throw new RuntimeException("Invalid module index");
     }
 
-    @Override
-    public void updateInputs(ModuleIOInputs inputs) {
-        inputs.drivePositionRad = Units.rotationsToRadians(driveEncoder.getPosition())
-                / SwerveConstants.DrivingMotorReduction;
-        inputs.driveVelocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(driveEncoder.getVelocity())
-                / SwerveConstants.DrivingMotorReduction;
-        inputs.driveAppliedVolts = driveSparkMax.getAppliedOutput() * driveSparkMax.getBusVoltage();
-        inputs.driveCurrentAmps = new double[] { driveSparkMax.getOutputCurrent() };
-        inputs.turnAbsolutePosition = Rotation2d.fromRotations(turnEncoder.getPosition()).minus(absoluteEncoderOffset);
-        inputs.turnVelocityRadPerSec = Units.rotationsToRadians(turnEncoder.getVelocity());
-        inputs.turnAppliedVolts = turnSparkMax.getAppliedOutput() * turnSparkMax.getBusVoltage();
-        inputs.turnCurrentAmps = new double[] { turnSparkMax.getOutputCurrent() };
-    }
+    driveSparkMax = new CANSparkMax(driveId, MotorType.kBrushless);
+    turnSparkMax = new CANSparkMax(turnId, MotorType.kBrushless);
+    absoluteEncoderOffset = Rotation2d.fromDegrees(angularOffsetDegrees);
 
-    @Override
-    public void setDriveVoltage(double volts) {
-        driveSparkMax.setVoltage(volts);
-    }
+    driveSparkMax.restoreFactoryDefaults();
+    turnSparkMax.restoreFactoryDefaults();
 
-    @Override
-    public void setTurnVoltage(double volts) {
-        turnSparkMax.setVoltage(volts);
-    }
+    driveSparkMax.setCANTimeout(250);
+    turnSparkMax.setCANTimeout(250);
 
-    @Override
-    public void setDriveBrakeMode(boolean enable) {
-        driveSparkMax.setIdleMode(enable ? IdleMode.kBrake : IdleMode.kCoast);
-    }
+    driveEncoder = driveSparkMax.getEncoder();
+    turnEncoder = turnSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
 
-    @Override
-    public void setTurnBrakeMode(boolean enable) {
-        turnSparkMax.setIdleMode(enable ? IdleMode.kBrake : IdleMode.kCoast);
-    }
+    turnSparkMax.setInverted(SwerveConstants.TurningEncoderInverted);
+    driveSparkMax.setSmartCurrentLimit(SwerveConstants.DrivingMotorCurrentLimit);
+    turnSparkMax.setSmartCurrentLimit(SwerveConstants.TurningMotorCurrentLimit);
+    driveSparkMax.enableVoltageCompensation(12.0);
+    turnSparkMax.enableVoltageCompensation(12.0);
+
+    driveEncoder.setPosition(0.0);
+    driveEncoder.setMeasurementPeriod(10);
+    driveEncoder.setAverageDepth(2);
+
+    turnEncoder.setAverageDepth(2);
+
+    driveSparkMax.setCANTimeout(0);
+    turnSparkMax.setCANTimeout(0);
+
+    driveSparkMax.setIdleMode(SwerveConstants.DrivingMotorIdleMode);
+    turnSparkMax.setIdleMode(SwerveConstants.TurningMotorIdleMode);
+
+    driveSparkMax.burnFlash();
+    turnSparkMax.burnFlash();
+  }
+
+  @Override
+  public void updateInputs(ModuleIOInputs inputs) {
+    inputs.drivePositionRad =
+        Units.rotationsToRadians(driveEncoder.getPosition())
+            / SwerveConstants.DrivingMotorReduction;
+    inputs.driveVelocityRadPerSec =
+        Units.rotationsPerMinuteToRadiansPerSecond(driveEncoder.getVelocity())
+            / SwerveConstants.DrivingMotorReduction;
+    inputs.driveAppliedVolts = driveSparkMax.getAppliedOutput() * driveSparkMax.getBusVoltage();
+    inputs.driveCurrentAmps = new double[] {driveSparkMax.getOutputCurrent()};
+    inputs.turnAbsolutePosition =
+        Rotation2d.fromRotations(turnEncoder.getPosition()).minus(absoluteEncoderOffset);
+    inputs.turnVelocityRadPerSec = Units.rotationsToRadians(turnEncoder.getVelocity());
+    inputs.turnAppliedVolts = turnSparkMax.getAppliedOutput() * turnSparkMax.getBusVoltage();
+    inputs.turnCurrentAmps = new double[] {turnSparkMax.getOutputCurrent()};
+  }
+
+  @Override
+  public void setDriveVoltage(double volts) {
+    driveSparkMax.setVoltage(volts);
+  }
+
+  @Override
+  public void setTurnVoltage(double volts) {
+    turnSparkMax.setVoltage(volts);
+  }
+
+  @Override
+  public void setDriveBrakeMode(boolean enable) {
+    driveSparkMax.setIdleMode(enable ? IdleMode.kBrake : IdleMode.kCoast);
+  }
+
+  @Override
+  public void setTurnBrakeMode(boolean enable) {
+    turnSparkMax.setIdleMode(enable ? IdleMode.kBrake : IdleMode.kCoast);
+  }
 }
--- src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
+++ src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
@@ -25,82 +25,86 @@ import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class Flywheel extends SubsystemBase {
-    private final FlywheelIO io;
-    private final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
-    private final SimpleMotorFeedforward ffModel;
-    private final SysIdRoutine sysId;
+  private final FlywheelIO io;
+  private final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
+  private final SimpleMotorFeedforward ffModel;
+  private final SysIdRoutine sysId;
 
-    /** Creates a new Flywheel. */
-    public Flywheel(FlywheelIO io) {
-        this.io = io;
+  /** Creates a new Flywheel. */
+  public Flywheel(FlywheelIO io) {
+    this.io = io;
 
-        // Switch constants based on mode (the physics simulator is treated as a
-        // separate robot with different tuning)
-        switch (Constants.currentMode) {
-        case REAL:
-        case REPLAY:
-            ffModel = new SimpleMotorFeedforward(0.1, 0.05);
-            io.configurePID(1.0, 0.0, 0.0);
-            break;
-        case SIM:
-            ffModel = new SimpleMotorFeedforward(0.0, 0.03);
-            io.configurePID(0.5, 0.0, 0.0);
-            break;
-        default:
-            ffModel = new SimpleMotorFeedforward(0.0, 0.0);
-            break;
-        }
-
-        // Configure SysId
-        sysId = new SysIdRoutine(
-                new SysIdRoutine.Config(null, null, null,
-                        (state) -> Logger.recordOutput("Flywheel/SysIdState", state.toString())),
-                new SysIdRoutine.Mechanism((voltage) -> runVolts(voltage.in(Volts)), null, this));
+    // Switch constants based on mode (the physics simulator is treated as a
+    // separate robot with different tuning)
+    switch (Constants.currentMode) {
+      case REAL:
+      case REPLAY:
+        ffModel = new SimpleMotorFeedforward(0.1, 0.05);
+        io.configurePID(1.0, 0.0, 0.0);
+        break;
+      case SIM:
+        ffModel = new SimpleMotorFeedforward(0.0, 0.03);
+        io.configurePID(0.5, 0.0, 0.0);
+        break;
+      default:
+        ffModel = new SimpleMotorFeedforward(0.0, 0.0);
+        break;
     }
 
-    @Override
-    public void periodic() {
-        io.updateInputs(inputs);
-        Logger.processInputs("Flywheel", inputs);
-    }
+    // Configure SysId
+    sysId =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                (state) -> Logger.recordOutput("Flywheel/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism((voltage) -> runVolts(voltage.in(Volts)), null, this));
+  }
 
-    /** Run open loop at the specified voltage. */
-    public void runVolts(double volts) {
-        io.setVoltage(volts);
-    }
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Flywheel", inputs);
+  }
 
-    /** Run closed loop at the specified velocity. */
-    public void runVelocity(double velocityRPM) {
-        var velocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(velocityRPM);
-        io.setVelocity(velocityRadPerSec, ffModel.calculate(velocityRadPerSec));
+  /** Run open loop at the specified voltage. */
+  public void runVolts(double volts) {
+    io.setVoltage(volts);
+  }
 
-        // Log flywheel setpoint
-        Logger.recordOutput("Flywheel/SetpointRPM", velocityRPM);
-    }
+  /** Run closed loop at the specified velocity. */
+  public void runVelocity(double velocityRPM) {
+    var velocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(velocityRPM);
+    io.setVelocity(velocityRadPerSec, ffModel.calculate(velocityRadPerSec));
 
-    /** Stops the flywheel. */
-    public void stop() {
-        io.stop();
-    }
+    // Log flywheel setpoint
+    Logger.recordOutput("Flywheel/SetpointRPM", velocityRPM);
+  }
 
-    /** Returns a command to run a quasistatic test in the specified direction. */
-    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
-        return sysId.quasistatic(direction);
-    }
+  /** Stops the flywheel. */
+  public void stop() {
+    io.stop();
+  }
 
-    /** Returns a command to run a dynamic test in the specified direction. */
-    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-        return sysId.dynamic(direction);
-    }
+  /** Returns a command to run a quasistatic test in the specified direction. */
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return sysId.quasistatic(direction);
+  }
 
-    /** Returns the current velocity in RPM. */
-    @AutoLogOutput
-    public double getVelocityRPM() {
-        return Units.radiansPerSecondToRotationsPerMinute(inputs.velocityRadPerSec);
-    }
+  /** Returns a command to run a dynamic test in the specified direction. */
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return sysId.dynamic(direction);
+  }
 
-    /** Returns the current velocity in radians per second. */
-    public double getCharacterizationVelocity() {
-        return inputs.velocityRadPerSec;
-    }
+  /** Returns the current velocity in RPM. */
+  @AutoLogOutput
+  public double getVelocityRPM() {
+    return Units.radiansPerSecondToRotationsPerMinute(inputs.velocityRadPerSec);
+  }
+
+  /** Returns the current velocity in radians per second. */
+  public double getCharacterizationVelocity() {
+    return inputs.velocityRadPerSec;
+  }
 }
