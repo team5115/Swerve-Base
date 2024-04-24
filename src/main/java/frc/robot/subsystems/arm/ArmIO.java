e2423f6c032c6b7c0a10575146eb960c7f5e0ebb
avidcoder27 <gilireeves@gmail.com>
2024-04-24 16:24:09 -0400

    real spark max gooders

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -1,12 +1,11 @@
 package frc.robot.subsystems.arm;
 
-import org.littletonrobotics.junction.AutoLog;
-
 import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
 
 public interface ArmIO {
     @AutoLog
-    public static class ArmIOInputs{
+    public static class ArmIOInputs {
         public Rotation2d armAngle = new Rotation2d();
         public double armVelocityRPM = 0;
         public double armCurrentAmps = 0;
@@ -14,5 +13,6 @@ public interface ArmIO {
     }
 
     public default void updateInputs(ArmIOInputs inputs) {}
+
     public default void setArmVoltage(double volts) {}
 }
