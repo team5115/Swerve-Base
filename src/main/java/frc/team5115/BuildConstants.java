b806bb434384e6a244feee802e1d17d9f30990d8
avidcoder27 <gilireeves@gmail.com>
2024-04-25 15:03:16 -0400

    rename `robot` folder to `team5115`

---
+++ src/main/java/frc/team5115/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "Swerve-Base-1";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 27;
+    public static final String GIT_SHA = "ba904d0ecff572c5f8366f2b4a7be542c4d8a6c4";
+    public static final String GIT_DATE = "2024-04-24 17:07:36 EDT";
+    public static final String GIT_BRANCH = "shooter";
+    public static final String BUILD_DATE = "2024-04-24 17:15:12 EDT";
+    public static final long BUILD_UNIX_TIME = 1713993312748L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
