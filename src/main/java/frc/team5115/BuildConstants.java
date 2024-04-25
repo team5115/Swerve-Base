97337db70e6a314a3599dfbaded75589cef284d0
avidcoder27 <gilireeves@gmail.com>
2024-04-25 15:22:06 -0400

    this is hte only wat

--- src/main/java/frc/team5115/BuildConstants.java
+++ src/main/java/frc/team5115/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Swerve-Base";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 31;
-    public static final String GIT_SHA = "1bfe6cf6bcd1bc402568949d952a627f27f1bd16";
-    public static final String GIT_DATE = "2024-04-25 15:04:44 EDT";
+    public static final int GIT_REVISION = 32;
+    public static final String GIT_SHA = "897358d65aad4b2645dca4c7c64cf025fa7cabf9";
+    public static final String GIT_DATE = "2024-04-25 15:17:09 EDT";
     public static final String GIT_BRANCH = "shooter";
-    public static final String BUILD_DATE = "2024-04-25 15:16:39 EDT";
-    public static final long BUILD_UNIX_TIME = 1714072599431L;
+    public static final String BUILD_DATE = "2024-04-25 15:21:40 EDT";
+    public static final long BUILD_UNIX_TIME = 1714072900734L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
