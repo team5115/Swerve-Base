f7ba57bb03e5ad586e662e1db86caa6d3c8d2df6
Idiakosa <kosa3200@gmail.com>
2024-10-31 08:08:41 -0400

    remember to prepare new flash drives for rios

--- src/main/java/frc/team5115/Robot.java
+++ src/main/java/frc/team5115/Robot.java
@@ -48,6 +48,12 @@ public class Robot extends LoggedRobot {
         switch (Constants.currentMode) {
             case REAL:
                 // Running on a real robot, log to a USB stick ("/U/logs")
+                // This creates the logs folder if it doesn't exist
+                // ! IMPORTANT: Remember, when using a new flash drive, make sure to do the following:
+                // 1) flash the drive for ext4, probably using a linux compute
+                // 2) set the permissions:
+                //      i) ssh into the rio using `ssh admin@10.51.15.2` (or whatever ip)  
+                //      ii) set the permission with chmod: `chmod 777 U/logs`
                 String path = "/U/logs";
                 var directory = new File(path);
                 if (!directory.exists()) {
